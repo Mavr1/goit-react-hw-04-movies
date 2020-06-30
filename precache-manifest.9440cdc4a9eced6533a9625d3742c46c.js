6a2d5d385b75fd5d990c926fe4ea958384d10df8
Mavr1 <mavrodiy.m.e@gmail.com>
2020-06-30 19:03:53 +0300

    Updates

---
+++ precache-manifest.9440cdc4a9eced6533a9625d3742c46c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab864cf2018ba401a18fb676b3ded7a3",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "d5f45d9735bf3df794d5",
+    "url": "/goit-react-hw-04-movies/static/css/main.f41840f2.chunk.css"
+  },
+  {
+    "revision": "95c96842eaaba65c23c8",
+    "url": "/goit-react-hw-04-movies/static/js/6.88f3775a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/6.88f3775a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10fe66de797fd9d735e",
+    "url": "/goit-react-hw-04-movies/static/js/CastPage.b4f449bf.chunk.js"
+  },
+  {
+    "revision": "0e72d03d3f2a8e0193f0",
+    "url": "/goit-react-hw-04-movies/static/js/HomePage.3c2470fe.chunk.js"
+  },
+  {
+    "revision": "420609f54a36920a8e32",
+    "url": "/goit-react-hw-04-movies/static/js/MovieDetailsPage.1d67877e.chunk.js"
+  },
+  {
+    "revision": "245a93f6c9883561c0ec",
+    "url": "/goit-react-hw-04-movies/static/js/ReviewsPage.67b4a331.chunk.js"
+  },
+  {
+    "revision": "d5f45d9735bf3df794d5",
+    "url": "/goit-react-hw-04-movies/static/js/main.449c0956.chunk.js"
+  },
+  {
+    "revision": "1a43541e5e62617de6ce",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.0b6c7c8f.js"
+  }
+]);
